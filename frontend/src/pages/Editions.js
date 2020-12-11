4972c233befc0566ce37f2feefd446ce2282cb3c
jordanlamantia <jordan.lamantia@eemi.com>
2020-12-11 12:21:29 +0100

    Page Edition ajout liste artistes

--- frontend/src/pages/Editions.js
+++ frontend/src/pages/Editions.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import '../css/Editions.css';
 
 function Editions() {
@@ -6,21 +7,30 @@ function Editions() {
         <div className="editionsContainer">
             <div className="listArtistsContainer">
                 <ul className="listArtists">
-                    <li>Artiste 1</li>
-                    <li>Artiste 2</li>
-                    <li>Artiste 3</li>
-                    <li>Artiste 4</li>
-                    <li>Artiste 5</li>
-                    <li>Artiste 5</li>
-                    <li>Artiste 5</li>
-                    <li>Artiste 5</li>
-                    <li>Artiste 5</li>
-                    <li>Artiste 5</li>
-                    <li>Artiste 5</li>
-                    <li>Artiste 5</li>
-                    <li>Artiste 5</li>
-                    <li>Artiste 5</li>
-                    <li>Artiste 5</li>
+                    <li><Link to="/artiste/1">Antoine Marquis</Link></li>
+                    <li><Link to="/artiste/1">Henri Michaux</Link></li>
+                    <li><Link to="/artiste/1">Rajak Ohanian</Link></li>
+                    <li><Link to="/artiste/1">Antonio Saura</Link></li>
+                    <li><Link to="/artiste/1">Gérard Traquandi</Link></li>
+                    <li><Link to="/artiste/1">Jean-Pierre Pincemin</Link></li>
+                    <li><Link to="/artiste/1">Eloïse Van der Heyden</Link></li>
+                    <li><Link to="/artiste/1">Balthasar Burkhard</Link></li>
+                    <li><Link to="/artiste/1">Antoine Marquis</Link></li>
+                    <li><Link to="/artiste/1">Henri Michaux</Link></li>
+                    <li><Link to="/artiste/1">Rajak Ohanian</Link></li>
+                    <li><Link to="/artiste/1">Antonio Saura</Link></li>
+                    <li><Link to="/artiste/1">Gérard Traquandi</Link></li>
+                    <li><Link to="/artiste/1">Jean-Pierre Pincemin</Link></li>
+                    <li><Link to="/artiste/1">Eloïse Van der Heyden</Link></li>
+                    <li><Link to="/artiste/1">Balthasar Burkhard</Link></li>
+                    <li><Link to="/artiste/1">Antoine Marquis</Link></li>
+                    <li><Link to="/artiste/1">Henri Michaux</Link></li>
+                    <li><Link to="/artiste/1">Rajak Ohanian</Link></li>
+                    <li><Link to="/artiste/1">Antonio Saura</Link></li>
+                    <li><Link to="/artiste/1">Gérard Traquandi</Link></li>
+                    <li><Link to="/artiste/1">Jean-Pierre Pincemin</Link></li>
+                    <li><Link to="/artiste/1">Eloïse Van der Heyden</Link></li>
+                    <li><Link to="/artiste/1">Balthasar Burkhard</Link></li>
                 </ul>
             </div>
             <div className="listPaints">
